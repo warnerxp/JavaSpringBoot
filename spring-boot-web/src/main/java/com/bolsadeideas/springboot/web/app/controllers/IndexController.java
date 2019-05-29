22879787d84c27c597475ec06f47a814730bb6a7
Wagner Alvarado <warnerxp@outlook.com>
2019-05-29 15:28:06 -0600

    if foreach thymeleaf

--- spring-boot-web/src/main/java/com/bolsadeideas/springboot/web/app/controllers/IndexController.java
+++ spring-boot-web/src/main/java/com/bolsadeideas/springboot/web/app/controllers/IndexController.java
@@ -1,5 +1,8 @@
 package com.bolsadeideas.springboot.web.app.controllers;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
@@ -33,9 +36,22 @@ public class IndexController {
 		Usuario usuario = new Usuario();
 		usuario.setNombre("Steve");
 		usuario.setApellido("Rogers");
+		usuario.setEmail("warnerxp@outlook.com");
 		model.addAttribute("usuario",usuario);
 		model.addAttribute("titulo","Perfil del usuario:".concat(usuario.getNombre()));
 		return "perfil";					
+	}
+	
+	@RequestMapping("/listar")
+	public String listar(Model model) 
+	{
+		List<Usuario> usuarios = new ArrayList();
+		usuarios.add(new Usuario("Clark","Kent","kent@aol.com"));
+		usuarios.add(new Usuario("Lex","Luthor","luthor@gmail.com"));
+		usuarios.add(new Usuario("Lois","Lane","lane@outlook.com"));
+		model.addAttribute("titulo","Listado de usuarios");
+		model.addAttribute("usuarios",usuarios);
+		return "listar";					
 	}
 	
 }
