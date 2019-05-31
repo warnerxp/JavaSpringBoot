e5e4394e42a5f29b8cab0a37bc90293fa29577af
Wagner Alvarado <warnerxp@outlook.com>
2019-05-30 20:15:56 -0600

    Path params request params

---
+++ spring-boot-web/src/main/java/com/bolsadeideas/springboot/web/app/controllers/EjemploVariablesRutaController.java
@@ -0,0 +1,35 @@
+package com.bolsadeideas.springboot.web.app.controllers;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/variables")
+public class EjemploVariablesRutaController 
+{
+	
+	@GetMapping("/")
+	public String index(Model model) 
+	{
+		model.addAttribute("titulo","Enviar parámetros de la ruta(@Path Variable)");
+		return "variables/index";
+	}
+	
+	@GetMapping("/string/{texto}")
+	public String variables(@PathVariable String texto,Model model ) 
+	{
+		model.addAttribute("titulo","Recibir parámetros de la ruta(@Path Variable)");
+		model.addAttribute("resultado","El texto enviado en la ruta es: "+ texto);
+		return "variables/ver";
+	}
+	
+	@GetMapping("/string/{texto}/{numero}")
+	public String variables(@PathVariable String texto,@PathVariable Integer numero,Model model ) 
+	{
+		model.addAttribute("titulo","Recibir parámetros de la ruta(@Path Variable)");
+		model.addAttribute("resultado","El texto enviado en la ruta es: "+ texto+" y el numero enviado en el path es: "+ numero);
+		return "variables/ver";
+	}
+}
